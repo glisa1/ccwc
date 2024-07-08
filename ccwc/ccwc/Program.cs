f634777a8917fa3ef86abf3b6f34af280a12c8ca
Danijel <glisovic.danijel96@gmail.com>
2024-07-08 15:20:47 +0200

    Added -c command.

--- ccwc/ccwc/Program.cs
+++ ccwc/ccwc/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿if (args.Length < 1)
+    return;
+
+if (args[0] == "-c")
+{
+    var inputFile = args[1] ?? throw new ArgumentException("File not provided as a parameter.");
+
+    var bytes = await File.ReadAllBytesAsync(inputFile);
+
+    if (bytes == null)
+        throw new Exception("File not found");
+
+    Console.WriteLine($"{bytes.Length} {inputFile}");
+}

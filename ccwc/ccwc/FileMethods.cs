d1e4bc8679c8fa20f1c2d78f2bd7cf55eba968bb
Danijel <glisovic.danijel96@gmail.com>
2024-07-10 11:12:19 +0200

    Refactored the code. Moved file methods to separate class.

---
+++ ccwc/ccwc/FileMethods.cs
@@ -0,0 +1,40 @@
+﻿namespace ccwc;
+
+internal sealed class FileMethods
+{
+    private string FileName { get; init; }
+
+    public FileMethods(string fileName)
+    {
+        if (!File.Exists(fileName))
+            throw new FileNotFoundException();
+
+        FileName = fileName;
+    }
+
+    public async Task<int> GetBytesCount()
+    {
+        var bytes = await File.ReadAllBytesAsync(FileName);
+        return bytes.Length;
+    }
+
+    public async Task<int> GetLinesCount()
+    {
+        var lines = await File.ReadAllLinesAsync(FileName);
+        return lines.Length;
+    }
+
+    public async Task<int> GetWordsCount()
+    {
+        var text = await File.ReadAllTextAsync(FileName);
+        var words = text.Split([' ', '\r', '\n', '\t'], StringSplitOptions.RemoveEmptyEntries);
+        return words.Length;
+    }
+
+    public async Task<int> GetCharactersCount()
+    {
+        var text = await File.ReadAllTextAsync(FileName);
+        var chars = text.ToCharArray();
+        return chars.Length;
+    }
+}
--- ccwc/ccwc/Program.cs
+++ ccwc/ccwc/Program.cs
@@ -1,32 +1,32 @@
-﻿using System.Text;
+﻿using ccwc;
 
 if (args.Length < 2)
     return;
 
 var inputFile = args[1] ?? throw new ArgumentException("File not provided as a parameter.");
 
+var fileMethods = new FileMethods(inputFile);
+
 if (args[0] == "-c")
 {
-    var bytes = await File.ReadAllBytesAsync(inputFile) ?? throw new Exception("File not found");
-    Console.WriteLine($"{bytes.Length} {inputFile}");
+    var bytesCount = await fileMethods.GetBytesCount();
+    Console.WriteLine($"{bytesCount} {inputFile}");
 }
 
 if (args[0] == "-l")
 {
-    var lines = await File.ReadAllLinesAsync(inputFile) ?? throw new Exception("File not found");
-    Console.WriteLine($"{lines.Length} {inputFile}");
+    var linesCount = await fileMethods.GetLinesCount();
+    Console.WriteLine($"{linesCount} {inputFile}");
 }
 
 if (args[0] == "-w")
 {
-    var text = await File.ReadAllTextAsync(inputFile) ?? throw new Exception("File not found");
-    var words = text.Split([' ', '\r', '\n', '\t'], StringSplitOptions.RemoveEmptyEntries);
-    Console.WriteLine($"{words.Length} {inputFile}");
+    var wordsCount = await fileMethods.GetWordsCount();
+    Console.WriteLine($"{wordsCount} {inputFile}");
 }
 
 if (args[0] == "-m")
 {
-    var text = await File.ReadAllTextAsync(inputFile) ?? throw new Exception("File not found");
-    var chars = text.ToCharArray();
-    Console.WriteLine($"{chars.Length} {inputFile}");
+    var charactersCount = await fileMethods.GetCharactersCount();
+    Console.WriteLine($"{charactersCount} {inputFile}");
 }

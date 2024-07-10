b8e738ef44dfa75677b1eacdb5afb0c713fe74f6
Danijel <glisovic.danijel96@gmail.com>
2024-07-10 11:33:47 +0200

    Implemented default command with just text file and without parameters.

--- ccwc/ccwc/Program.cs
+++ ccwc/ccwc/Program.cs
@@ -1,32 +1,48 @@
 ﻿using ccwc;
 
-if (args.Length < 2)
+if (args.Length == 0)
     return;
 
-var inputFile = args[1] ?? throw new ArgumentException("File not provided as a parameter.");
-
-var fileMethods = new FileMethods(inputFile);
-
-if (args[0] == "-c")
+if (args.Length == 1)
 {
+    var inputFile = args[0];
+    var fileMethods = new FileMethods(inputFile);
     var bytesCount = await fileMethods.GetBytesCount();
-    Console.WriteLine($"{bytesCount} {inputFile}");
-}
-
-if (args[0] == "-l")
-{
     var linesCount = await fileMethods.GetLinesCount();
-    Console.WriteLine($"{linesCount} {inputFile}");
-}
-
-if (args[0] == "-w")
-{
     var wordsCount = await fileMethods.GetWordsCount();
-    Console.WriteLine($"{wordsCount} {inputFile}");
-}
 
-if (args[0] == "-m")
+    Console.WriteLine($"{linesCount} {wordsCount} {bytesCount} {inputFile}");
+
+}
+else if (args.Length == 2)
 {
-    var charactersCount = await fileMethods.GetCharactersCount();
-    Console.WriteLine($"{charactersCount} {inputFile}");
+    var inputFile = args[1];
+    var fileMethods = new FileMethods(inputFile);
+
+    if (args[0] == "-c")
+    {
+        var bytesCount = await fileMethods.GetBytesCount();
+        Console.WriteLine($"{bytesCount} {inputFile}");
+    }
+
+    else if (args[0] == "-l")
+    {
+        var linesCount = await fileMethods.GetLinesCount();
+        Console.WriteLine($"{linesCount} {inputFile}");
+    }
+
+    else if (args[0] == "-w")
+    {
+        var wordsCount = await fileMethods.GetWordsCount();
+        Console.WriteLine($"{wordsCount} {inputFile}");
+    }
+
+    else if (args[0] == "-m")
+    {
+        var charactersCount = await fileMethods.GetCharactersCount();
+        Console.WriteLine($"{charactersCount} {inputFile}");
+    }
+
+    else
+        Console.WriteLine("Command not supported.");
 }
